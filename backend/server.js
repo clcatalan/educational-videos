b43e02848af325ebc7e8bec7709511385bcbe839
prak0310 <prakritimuralidharan@gmail.com>
2026-07-17 19:08:49 +0800

    fixing BE

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const assignmentRoutes = require("./routes/assignments");
 
 // Middleware
 app.use(cors());
@@ -70,10 +71,14 @@ app.get('/api/categories', async (req, res) => {
   }
 });
 
+app.use("/api/playlists", playlistRoutes);
+app.use("/api/assignments", assignmentRoutes);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
-app.use("/api/playlists", playlistRoutes);
+
 
